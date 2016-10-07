710284c81dea027f806fe128d9bf88b91e2c6f5f
jrgleason <jackiegleason@gmail.com>
2016-10-07 12:35:20 -0400

    Second Step: Trying to get step 3 working but running into a webpack issue

--- src/main/ng1/demo.coffee
+++ src/main/ng1/demo.coffee
@@ -1,5 +1,13 @@
+
+{ upgradeAdapter, NgAppComponent } = require '../ng2/upgrade.adapter.ts';
 (->
+
+
   module = angular.module 'simpleApp', []
+  angular.element(document).ready ->
+    upgradeAdapter.bootstrap(document.body, ['simpleApp']);
+    module.directive 'jgType',  NgAppComponent
+
   module.directive 'jgCoffee', -> {
     scope: {},
     template: String(require('./demo.html'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 var webpack = require("webpack");
 var path = require("path");
 var glob = require('glob-all');
-var _root = path.resolve(__dirname, '..');
-function root(args) {
-    args = Array.prototype.slice.call(arguments, 0);
-    return path.join.apply(path, [_root].concat(args));
-}
 var old = glob.sync([
   './dependencies/ng1.coffee',
   './src/main/ng1/**/*.coffee',
@@ -19,8 +14,8 @@ module.exports = {
     context: __dirname,
     entry: {
         polyfills: './dependencies/polyfills.ts',
-        ng1: old,
-        ng2: update
+        ngnew: update,
+        ngold: old
     },
     resolve: {
         extensions: ['', '.ts', '.coffee', '.html', '.js']
@@ -32,14 +27,14 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.ts$/, loader: 'ts' },
-            { test: /\.coffee$/, loaders: ["coffee-loader"]},
+            { test: /\.coffee$/, loaders: ["coffee-loader", "coffee-import"]},
             { test: /\.html$/, loader: "html" }
         ]
     },
     devtool: '#inline-source-map',
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
-            names: ['ng1', 'ng2', 'polyfills'],
+            names: ['ngold', 'ngnew', 'polyfills'],
             minChunks: Infinity
         })
     ]
